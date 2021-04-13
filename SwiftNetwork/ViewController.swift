27c4d222f4c87775eb5787870ada6eaae725c7ea
Eddy <eddylujann@gmail.com>
2021-04-13 18:40:31 +0200

    Initial Commit

---
+++ SwiftNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftNetwork
+//
+//  Created by Eddy Donald Chinchay Lujan on 13/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
